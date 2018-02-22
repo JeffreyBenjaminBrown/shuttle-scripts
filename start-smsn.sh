cff19b30471665af5aa298bd632c78acc84a1270
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2018-02-22 15:33:07 -0500

    prompt user for custom docker container variables, if desired

--- start-smsn.sh
+++ start-smsn.sh
@@ -1,3 +1,20 @@
-sudo docker run --name smsn -it -v /home/jeff/Encfs/kb-not-encfs:/mnt/smsn-data -p 8182:8182 -d -h 127.0.0.1 jeffreybbrown/smsn-develop:2017-11-24
+#!/bin/bash
 
-sudo docker exec -it smsn bash
+default_name=smsn
+printf "What name to give the new smsn container? (Default: $default_name.)\n"
+read name
+name=${name:-$default_name}
+
+default_port=8182
+printf "\nWhat port to serve the new smsn container on? (Default: $default_port.)\n"
+read port
+port=${port:-$default_port}
+
+default_folder=/home/jeff/Encfs/kb-not-encfs
+printf "\nWhat folder to mount? (Default: $default_folder.)\n"
+read folder
+folder=${folder:-$default_folder}
+
+sudo docker run --name $name -it -v /home/jeff/Encfs/kb-not-encfs:/mnt/smsn-data -p $port:8182 -d -h 127.0.0.1 jeffreybbrown/smsn-develop:2017-11-24
+
+sudo docker exec -it $name bash
