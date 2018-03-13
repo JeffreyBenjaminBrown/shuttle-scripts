4a9956bcf961b29000604f59e6dc242b713271e3
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2018-03-12 19:53:13 -0500

    make the warning to close emacs-smsn louder (red)

--- 4.rename.sh
+++ 4.rename.sh
@@ -4,4 +4,4 @@ mv kb-not-encfs old,moved-to-shuttle/$(date +"%Y_%m_%d_%T")
 mkdir kb-not-encfs
 tree -L 2
 
-printf "\n\nNow manually CLOSE ALL EMACS SMSN BUFFERS, to avoid conflicts from pushing a stale view later."
+echo -e "\033[31m\n\nNow manually CLOSE ALL EMACS SMSN BUFFERS, to avoid conflicts from pushing a stale view later.\n\033[0m"
